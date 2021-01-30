2d35fefd3a66848aca60ddeda2f8c79d46104b4d
kiruthika20 <76843167+kiruthika20@users.noreply.github.com>
2021-01-30 12:01:36 +0530

    Update Front.js

--- Front.js
+++ Front.js
@@ -1,11 +1,11 @@
 import React,{useState} from 'react'
 import "./App.css"
-import Count from "./count"
+
 
 
 
 export default function Front() {
-  const [number,setNum]=useState("");
+  const [number,setNum]=  useState("");
  var[getNum,setnum]=useState("");
   
    function onclickHandle () {
@@ -16,16 +16,41 @@ export default function Front() {
     setNum(e.target.value)
     
     }
+    
+
+  var [count, setCount] = useState("");
+ 
+  function increase() {
+    if (count <10) {
+      setCount(count +1);
+    } else {
+      setCount(0);
+    }
+  }
+  function decrease() {
+    if (count > 1) {
+      setCount(count-1);
+    } else {
+      setCount(1);
+    }
+  }
+  var b=Number(getNum);
+  var c=Number(count);
+  var a=Number (b+c);
  
     return (
         <div >
          
             <input type="text" name="name"placeholder="type number.." onChange={Handleonchange} />
             <button onClick={onclickHandle}>Submit</button>
-      
+     
         
-        <h1>{getNum}</h1>
-        <Count />
+        <h1>{a}</h1>
+        <div >
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+      </div>
+      
         </div>
 
 
